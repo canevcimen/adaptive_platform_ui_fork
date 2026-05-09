2d35480eb2d1e3dda57023fcae2ad361975eae85
Yahya Can Evcimen <canevcimen@chargesquare.com>
2026-05-09 20:56:18 +0300

    feat: hide label and shift icon down when label is empty/whitespace

--- ios/Classes/iOS26TabBarPlatformView.swift
+++ ios/Classes/iOS26TabBarPlatformView.swift
@@ -199,6 +199,12 @@ class iOS26TabBarPlatformView: NSObject, FlutterPlatformView, UITabBarController
                     item.badgeValue = nil
                 }
 
+                // Label boş/space ise ikonu aşağı kaydır (label alanını kapat)
+                if let t = title, t.trimmingCharacters(in: .whitespaces).isEmpty {
+                    item.imageInsets = UIEdgeInsets(top: 6, left: 0, bottom: -6, right: 0)
+                    item.titlePositionAdjustment = UIOffset(horizontal: 0, vertical: 100) // label'ı ekran dışına it
+                }
+
                 items.append(item)
             }
             return items
